9e4b38b86446807b7ff509331f060f3aa08be11d
Aleksandr <gudwinao88@gmail.ru>
2022-10-06 13:42:49 +0400

    lesson_1

---
+++ todo/authors/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+# Create your views here.
+from .serializers import AuthorModelSerializer
+from .models import Author
+
+class Author(ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorModelSerializer
+
--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from rest_framework.routers import DefaultRouter
+from authors.views import AuthorModelViewSet
+
+router = DefaultRouter()
+router.register('authors', AuthorModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
 ]
