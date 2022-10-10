31a8be0b2c77d64101964bfa2c954c8729ffc6ca
Aleksandr <gudwinao88@gmail.ru>
2022-10-10 14:36:33 +0400

    Lesson_2_front_authors

--- todo/frontend/src/App.js
+++ todo/frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AuthorList from './components/Author.js'
+
+
 class App extends React.Component {
 
   constructor(props) {
@@ -11,10 +14,32 @@ class App extends React.Component {
 
   }
 
+  componentDidMount() {
+      const authors = [
+        {
+          'first_name': 'Фёдор',
+          'last_name': 'Достоевский',
+          'birthday_year': 1821
+        },
+        {
+          'first_name': 'Александр',
+          'last_name': 'Грин',
+          'birthday_year': 1880
+        },
+      ]
+
+      this.setState(
+        {
+          'authors': authors
+        }
+      )
+  }
+
+
   render () {
     return (
       <div>
-        Main App
+        <AuthorList authors={this.state.authors} />
       </div>
     ) 
   }
