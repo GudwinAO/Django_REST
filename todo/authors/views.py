582ad5da13a5464f10885a05e9e963f024603c34
Aleksandr <gudwinao88@gmail.ru>
2022-10-10 15:00:37 +0400

    lesson_2_fix_bags_lesson_1

--- todo/authors/views.py
+++ todo/authors/views.py
@@ -5,7 +5,7 @@ from rest_framework.viewsets import ModelViewSet
 from .serializers import AuthorModelSerializer
 from .models import Author
 
-class Author(ModelViewSet):
+class AuthorModelViewSet(ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorModelSerializer
 
