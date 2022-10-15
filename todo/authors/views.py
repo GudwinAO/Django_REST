cfec46d0ac999127a9671b2969459cb5f0037fae
Aleksandr <gudwinao88@gmail.ru>
2022-10-15 22:00:28 +0400

    lesson_3_done

--- todo/authors/views.py
+++ todo/authors/views.py
@@ -2,10 +2,21 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
-from .serializers import AuthorModelSerializer
-from .models import Author
+from .serializers import AuthorSerializer, BookSerializer, BiographySerializer, ArticleSerializer
+from .models import Author, Book, Biography, Article
 
 class AuthorModelViewSet(ModelViewSet):
     queryset = Author.objects.all()
-    serializer_class = AuthorModelSerializer
+    serializer_class = AuthorSerializer
 
+class BookModelViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BiographyModelViewSet(ModelViewSet):
+    queryset = Biography.objects.all()
+    serializer_class = BiographySerializer
+
+class ArticleModelViewSet(ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
