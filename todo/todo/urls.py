b12583107f2fe765f917c4d77466b4566ca1697c
Aleksandr <gudwinao88@gmail.ru>
2022-10-24 16:29:16 +0400

    Lesson_6_authtoken

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
