5a73f55c464794ed03f114f566bac6fe929db176
GudwinAO <92263753+GudwinAO@users.noreply.github.com>
2022-10-29 16:28:53 +0400

    lesson_6_done (#6)
    Co-authored-by: Aleksandr <gudwinao88@gmail.ru>

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from rest_framework.authtoken import views
+
 from authors.views import AuthorModelViewSet, BookModelViewSet, BiographyModelViewSet, ArticleModelViewSet
 
 
@@ -15,5 +16,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
+    #Lesson_6_token
     path('api-token-auth/', views.obtain_auth_token),
 ]
