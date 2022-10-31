78652045bb436b4ddb4896b2e0f4486363cdcae1
Aleksandr <gudwinao88@gmail.ru>
2022-10-31 17:39:04 +0400

    Lesson_8_APIRequestFactory

--- todo/authors/tests.py
+++ todo/authors/tests.py
@@ -1,3 +1,36 @@
+import json
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
+from mixer.backend.django import mixer
+from django.contrib.auth.models import User
+from .views import AuthorViewSet
+from .models import Author, Book
 
-# Create your tests here.
+
+class TestAuthorViewSet(TestCase):
+    def test_get_list(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/authors/')
+        view = AuthorViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_guest(self):
+        factory = APIRequestFactory()
+        request = factory.post('/api/authors/', {'name': 'Пушкин',
+        'birthday_year': 1799}, format='json')
+        view = AuthorViewSet.as_view({'post': 'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_admin(self):
+        factory = APIRequestFactory()
+        request = factory.post('/api/authors/', {'name': 'Пушкин',
+        'birthday_year': 1799}, format='json')
+        admin = User.objects.create_superuser('admin', 'admin@admin.com',
+        'admin123456')
+        force_authenticate(request, admin)
+        view = AuthorViewSet.as_view({'post': 'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
