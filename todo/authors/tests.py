ba77e36d33bc57b7eeefa6338348b07c5684929a
Aleksandr <gudwinao88@gmail.ru>
2022-11-07 17:50:51 +0400

    lesson_10_schema_book_author

--- todo/authors/tests.py
+++ todo/authors/tests.py
@@ -72,7 +72,7 @@ class TestAuthorViewSet(TestCase):
             response = self.client.get('/api/books/')
             self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        """def test_edit_admin(self):
+        def test_edit_admin(self):
             author = Author.objects.create(name='Пушкин', birthday_year=1799)
             book = Book.objects.create(name='Пиковая дама', author=author)
             admin = User.objects.create_superuser('admin', 'admin@admin.com',
@@ -81,7 +81,7 @@ class TestAuthorViewSet(TestCase):
             response = self.client.put(f'/api/books/{book.id}/', {'name': 'Руслан и Людмила', 'author': book.author.id})
             self.assertEqual(response.status_code, status.HTTP_200_OK)
             book = Book.objects.get(id=book.id)
-            self.assertEqual(book.name,'Руслан и Людмила')"""
+            self.assertEqual(book.name,'Руслан и Людмила')
 
         def test_edit_mixer(self):
             book = mixer.blend(Book)
--- todo/todo/schema.py
+++ todo/todo/schema.py
@@ -1,8 +1,29 @@
 import graphene
+from graphene_django import DjangoObjectType
+from authors.models import Book, Author
+
+
+class BookType(DjangoObjectType):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+
+class AuthorType(DjangoObjectType):
+    class Meta:
+        model = Author
+        fields = '__all__'
 
 
 class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hi!")
+    all_books = graphene.List(BookType)
+
+    def resolve_all_books(root, info):
+        return Book.objects.all()
+
+
+
 
 
 schema = graphene.Schema(query=Query)
+
